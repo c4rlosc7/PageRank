509b56fc399165afb60be099558e9237252817b4
c4rlosc7 <c4rlosc7>
2015-11-20 14:33:19 -0500

    update 1.1

--- centralizado/pageRank.cc
+++ centralizado/pageRank.cc
@@ -72,18 +72,62 @@ void fixgraph (Graph& g, NodeSet nodes){
 }
 
 int main(){
-  pair<Graph,NodeSet> g = readGraph("wikivote.txt");
+  pair<Graph,NodeSet> g = readGraph("example.txt");
   Graph graph = g.first; 
   NodeSet nodes = g.second;  
-  cout << graph.size() << endl;
-  cout << nodes.size() << endl;
+  cout << "nodos relacionados " << graph.size() << endl;
+  cout << "numero de nodos " << nodes.size() << endl;
   fixgraph(graph,nodes);
-  cout << graph.size() << endl;
-  cout << nodes.size() << endl;
+  //cout << graph.size() << endl;
+  //cout << nodes.size() << endl;
   
   pair<AdjMat,Norm> m = toMatrix(g.first,g.second);
-  int i = m.second[3];
-  int j = m.second[604];
-  cout << m.first[i][j] << endl;
+  int sum=0, ii=0;
+  double Lp[graph.size()];
+  double PrNew[graph.size()];
+  double d = 0.05;
+  double prI[graph.size()];
+  printf("%f\n", d);
+/*______________________________________________________________________________________*/    
+  for(int i=0; i<graph.size(); i++){
+  	if(i== 2){ prI[i]=1; }else{ prI[i]=0; }
+  }
+/*______________________________________________________________________________________*/  
+  for(int i=0; i<graph.size(); i++){
+  	for (int j=0; j<nodes.size(); j++){
+  		 sum = sum + m.first[i][j];
+  		 cout << m.first[i][j];
+  	}
+  	cout <<"\n";
+  	Lp[ii] = sum;	
+  	sum=0;
+  	ii++;
+  }
+
+  for(int x=0; x<graph.size(); x++){
+  	cout << Lp[x] <<endl;
+  }
+/*______________________________________________________________________________________*/  
+double aux;
+int ite=0;
+
+do{
+
+  for(int j=0; j<graph.size(); j++){
+  	for (int i=0; i<nodes.size(); i++){
+  		aux = (prI[i] / Lp[i]) * m.first[i][j];  	
+  		//printf("aux %f\n", aux);  		
+  	}  		 
+  	d = d + 0.85 * aux;
+  	PrNew[j]=d;
+  	d = 0.05;
+  	cout << PrNew[j] << endl;
+  	//prI[j] = PrNew[j];
+  	aux = 0.0;
+  }
+  for (int j = 0; j < 3; j++) { prI[j] = PrNew[j]; }  
+  ite++;
+}while(ite < 2);
+
   return 0;
 }
